384c8acbab30953b597a5955e60983f6f2581ca9
Guilherme Rodrigues Costa <rodriguesguidev.com>
2025-02-15 11:01:35 -0300

    Adiciona variáveis de ambiente e ajusta URL da API

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
-import "dotenv/config"
 
-const {NODE_ENV} = process.env
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// Use import.meta.env.MODE para acessar o ambiente
+const URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
 export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
-
 
 // console.log(responseArtists.data);
